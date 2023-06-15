b41986f2afd3f2d8c5201c1d918007d9a0ab5d6f
Manu Paiva <manu.paiva73@gmail.com>
2023-06-15 19:52:48 -0300

    change on config.js

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 export default {
   PORT: process.env.PORT || 3000,
-  AUTHOR: process.env.AUTHOR || 'Jero',
+  AUTHOR: process.env.AUTHOR || 'Your name',
   DB_URI: process.env.DB_URI || 'mongodb://localhost:27017/tripsdb',
 };
